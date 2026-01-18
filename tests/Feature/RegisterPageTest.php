023dc61b9acbe0692f1f8ab397068be54c0a6832
JaafarAljamal <jaafar.aljamal@gmail.com>
2026-01-18 21:50:36 +0300

    Add failing test for register form fields

--- tests/Feature/RegisterPageTest.php
+++ tests/Feature/RegisterPageTest.php
@@ -9,7 +9,17 @@ class RegisterPageTest extends TestCase
     public function test_register_page_is_accessible(): void
     {
         $response = $this->get('/register');
-        $response->assertStatus(200);
-        $response->assertSee('Register');
+        $response -> assertStatus(200);
+        $response -> assertSee('Register');
+    }
+
+    public function test_register_page_fields_are_visible(): void
+    {
+        $response = $this -> get('/register');
+
+        $response -> assertSee('Name');
+        $response -> assertSee('Email');
+        $response -> assertSee('Password');
+        $response -> assertSee('Register');
     }
 }
