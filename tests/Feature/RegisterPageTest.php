cd80a882816cf7ee4a8ce4628eac47b6346ae254
JaafarAljamal <jaafar.aljamal@gmail.com>
2026-01-18 03:03:40 +0300

    Add failing test for register page

---
+++ tests/Feature/RegisterPageTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class RegisterPageTest extends TestCase
+{
+    public function test_register_page_is_accessible(): void
+    {
+        $response = $this->get('/register');
+        $response->assertStatus(200);
+        $response->assertSee('Register');
+    }
+}
