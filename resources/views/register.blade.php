092494033f8b20b04276d52a4eeeef861c59989c
JaafarAljamal <jaafar.aljamal@gmail.com>
2026-01-19 00:54:56 +0300

    Implement register form fields to pass test

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,1 +1,16 @@
 <h1>Register</h1>
+
+<form method="POST" action="/register">
+    @csrf
+
+    <label for="name">Name</label>
+    <input type="text" id="name" name="name">
+
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email">
+
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+
+    <button type="submit">Register</button>
+</form>
