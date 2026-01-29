96f438d799b1105d1f1b039ee82462e5198edbb2
JaafarAljamal <jaafar.aljamal@gmail.com>
2026-01-29 22:33:47 +0300

    Apply PRS-12 formatting standards

--- database/migrations/2026_01_15_194105_create_users_table.php
+++ database/migrations/2026_01_15_194105_create_users_table.php
@@ -12,11 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table -> id();
-            $table -> string('username') -> unique();
-            $table -> string('email') -> unique();
-            $table -> string('password');
-            $table -> timestamps();
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
         });
     }
 
