7cc40632d0333a100eb5c6982655d2aa65900b87
JaafarAljamal <jaafar.aljamal@gmail.com>
2026-01-30 12:18:46 +0300

    Fix: use RegisterRequest in RegisterController@store to apply validation consistently

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
-    //
+    public function store(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        User::create([
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return redirect(route('home'));
+    }
 }
