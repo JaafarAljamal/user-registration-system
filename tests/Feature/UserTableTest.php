28df432e568edbc4ce462a0925ec449dc2eb7312
JaafarAljamal <jaafar.aljamal@gmail.com>
2026-01-30 16:11:21 +0300

    Add PHPUnit Test attribute import

--- tests/Feature/UserTableTest.php
+++ tests/Feature/UserTableTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class UserTableTest extends TestCase
@@ -10,13 +11,13 @@ class UserTableTest extends TestCase
     #[Test]
     public function users_table_has_expected_columns()
     {
-        $this -> assertTrue(Schema::hasTable('users'));
+        $this->assertTrue(Schema::hasTable('users'));
 
-        $this -> assertTrue(Schema::hasColumn('users', 'id'));
-        $this -> assertTrue(Schema::hasColumn('users', 'username'));
-        $this -> assertTrue(Schema::hasColumn('users', 'email'));
-        $this -> assertTrue(Schema::hasColumn('users', 'password'));
-        $this -> assertTrue(Schema::hasColumn('users', 'created_at'));
-        $this -> assertTrue(Schema::hasColumn('users', 'updated_at'));
+        $this->assertTrue(Schema::hasColumn('users', 'id'));
+        $this->assertTrue(Schema::hasColumn('users', 'username'));
+        $this->assertTrue(Schema::hasColumn('users', 'email'));
+        $this->assertTrue(Schema::hasColumn('users', 'password'));
+        $this->assertTrue(Schema::hasColumn('users', 'created_at'));
+        $this->assertTrue(Schema::hasColumn('users', 'updated_at'));
     }
 }
